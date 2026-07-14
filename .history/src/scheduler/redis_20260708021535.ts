3ad0e06f6c1afc2b178461fce35c9d7e023d7133
liudmyla122 <afina.palada555@gmail.com>
2026-07-14 04:43:31 -0700

    Remove .history and .lh folders

--- .history/src/scheduler/redis_20260708021535.ts
+++
@@ -1,7 +0,0 @@
-import IORedis from "ioredis";
-import { config } from "../config";
-
-// BullMQ требует maxRetriesPerRequest: null для стабильной работы очередей
-export const redisConnection = new IORedis(config.redisUrl, {
-  maxRetriesPerRequest: null,
-});
--- .history/src/scheduler/redis_20260710043635.ts
+++
@@ -1,9 +0,0 @@
-import IORedis from 'ioredis'
-import { config } from '../config'
-
-// BullMQ требует maxRetriesPerRequest: null для стабильной работы очередей
-// Приведение к any: BullMQ поставляет собственную копию ioredis, из-за чего
-// тип корня ioredis из node_modules конфликтует с ожидаемым ConnectionOptions.
-export const redisConnection = new IORedis(config.redisUrl, {
-  maxRetriesPerRequest: null,
-}) as any
